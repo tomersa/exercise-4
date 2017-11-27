bf21c9ba8ad25a0c4952296491482b28ed7f608b
Tomer Saban <sab30226@gmail.com>
2017-11-27 12:01:37 +0200

    step 5: implemented IAsyncTaskEvents in AsyncTaskActivity

--- app/src/main/java/com/tomer/exercise4/AsyncTaskActivity.java
+++ app/src/main/java/com/tomer/exercise4/AsyncTaskActivity.java
@@ -1,13 +1,28 @@
 package com.tomer.exercise4;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class AsyncTaskActivity extends AppCompatActivity {
+public class AsyncTaskActivity extends AppCompatActivity implements IAsyncTaskEvents{
+
+    TextView terminalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task);
+        terminalTextView = findViewById(R.id.terminal_tv);
+    }
+
+    @Override
+    public void onPostExecute() {
+        terminalTextView.setText(Resources.getSystem().getText(R.string.terminal_finished));
+    }
+
+    @Override
+    public void onProgressUpdate(Integer count) {
+        terminalTextView.setText(Resources.getSystem().getText(count));
     }
 }
