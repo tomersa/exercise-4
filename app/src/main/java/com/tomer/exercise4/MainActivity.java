f8360048d5a23d76c5a43ce5959d5e40804cfd70
Tomer Saban <sab30226@gmail.com>
2017-11-27 13:35:22 +0200

    Added functionality to the async task button

--- app/src/main/java/com/tomer/exercise4/MainActivity.java
+++ app/src/main/java/com/tomer/exercise4/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tomer.exercise4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.async_task_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AsyncTaskActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
