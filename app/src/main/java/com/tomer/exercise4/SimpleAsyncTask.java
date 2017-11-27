74a9a145453c830694c80045fa1e1f794249e9a0
Tomer Saban <sab30226@gmail.com>
2017-11-27 17:59:19 +0200

    Advanced step 2: Added methods and cancelled flag.

---
+++ app/src/main/java/com/tomer/exercise4/SimpleAsyncTask.java
@@ -0,0 +1,33 @@
+package com.tomer.exercise4;
+
+import android.os.AsyncTask;
+
+public abstract class SimpleAsyncTask extends AsyncTask<Void, Void, Void> {
+
+    private boolean cancelled = false;
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+}
