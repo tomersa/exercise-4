5384eff6e3f9d7f6023037cfaab649014e28c6ce
Tomer Saban <sab30226@gmail.com>
2017-11-27 09:40:04 +0200

    Added ActivityAsyncTask with top buttons

---
+++ app/src/main/java/com/tomer/exercise4/AsyncTaskActivity.java
@@ -0,0 +1,13 @@
+package com.tomer.exercise4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsyncTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task);
+    }
+}
